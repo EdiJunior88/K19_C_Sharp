b806595d499e3623619ccfe4b91aa030a89ff395
EdiJunior88 <er.moura88@yahoo.com.br>
2020-12-22 16:08:59 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_Codigo_2.5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Codigo_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
